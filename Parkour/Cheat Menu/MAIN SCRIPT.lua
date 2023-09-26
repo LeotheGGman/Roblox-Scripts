fb10257da73df6bffe60201b8837f45c870e24d1
LeotheGGman <139161922+LeotheGGman@users.noreply.github.com>
2023-09-26 16:08:29 -0600

    Update MAIN SCRIPT.lua

--- Parkour/Cheat Menu/MAIN SCRIPT.lua
+++ Parkour/Cheat Menu/MAIN SCRIPT.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Bag%20ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Parkour/Cheat%20Menu/Functions%20and%20other%20stuff.lua"))()
