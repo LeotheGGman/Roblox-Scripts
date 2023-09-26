d6be3603522dd2b471739f75c5c9c39f36f1ff30
LeotheGGman <139161922+LeotheGGman@users.noreply.github.com>
2023-09-26 16:38:46 -0600

    Update Auto Farm.lua

--- Parkour/Auto Farm.lua
+++ Parkour/Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Parkour/AutoFarm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Parkour/IGNORE%20THIS/AutoFarm.lua"))()
