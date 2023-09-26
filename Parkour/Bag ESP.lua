4620e063939ac64c539e29d313c8ea37d3c8b92f
LeotheGGman <139161922+LeotheGGman@users.noreply.github.com>
2023-09-26 16:20:28 -0600

    Update Bag ESP.lua

--- Parkour/Bag ESP.lua
+++ Parkour/Bag ESP.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Bag%20ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Parkour/IGNORE%20THIS/Bag%20ESP%20main.lua"))()
