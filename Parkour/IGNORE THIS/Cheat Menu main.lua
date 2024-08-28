59475cfdc3868bc6bba32dbe76a75d6099c550c4
LeotheGGman <139161922+LeotheGGman@users.noreply.github.com>
2024-08-28 15:57:54 -0600

    Update Cheat Menu main.lua

--- Parkour/IGNORE THIS/Cheat Menu main.lua
+++ Parkour/IGNORE THIS/Cheat Menu main.lua
@@ -3,15 +3,14 @@ local getmetatable = (debug.getmetatable or getrawmetatable);
 local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunction(getrawmetatable(tbl)[mt], func) end;
 
 repeat wait() until game:IsLoaded();
- --> LeotheGGman? He is a good guy. Sadly that he decided to publish all his codes and scripts.
+ --> LeotheGGman? I am lost.
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/LeotheGGman/Roblox-Scripts/main/Parkour/IGNORE%20THIS/TabUI.lua"))();
 local players = game:GetService("Players");
 local replicatedStorage = game:GetService("ReplicatedStorage");
 local scriptContext = game:GetService("ScriptContext");
 local client = players.LocalPlayer;
 local variables, mainEnv, encrypt;
---> What is his discord?
---> leotheggman - Talk to him. You may gain some interesting information about game!
+
 do
     local banRemotes = {
         "AttemptTeleport";
@@ -27,10 +26,10 @@ do
         "t";
         "FF";
     };
---> From Leo: It took me 6 years to play Parkour. It was such a nice time doing that! Bans, appeals, levels, updates... Years went through. 
---> And whats now? Well, I left game. I tried playing modded parkour, but... owners were sucks. I also tried to create my own parkour modded (find in my bio in roblox, nickname: ggggggggg8027)
+--> From Leo: It took me 5 years to play Parkour. It was such a nice time doing that! Bans, appeals, levels, updates... Years went through. 
+--> And whats now? Well, I left game. I tried playing modded parkour, but... owners were sucks. I also tried to create my own parkour modded
 --> Now i got older, and got other things to do. But im also waiting for Parkour Reborn. And will play that game a lot, at least i hope so ;D
---> Also look for other scripts that i have for parkour. Like bag ESP, its been undetected for 6 years, i think its safe to use it! :)
+--> Also look for other scripts that i have for parkour. Like bag ESP, its been undetected for 5 years, i think its safe to use it! :)
 --> Time went... So fast?
 --> And the last thing i want to say: Have fun using my scripts! Also feel free to contact me on discord: leotheggman
 --> Have a nice day... or night. I hope that you will succeed in your life and will send me couple bucks for that fun that my scripts gave you! ;D
